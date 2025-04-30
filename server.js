8157e90eb62cfd16d0487537d94f9d6e26b9f6ef
12yasaswi56 <yasaswikopparapu624@gmail.com>
2025-04-30 16:43:55 +0530

    FC

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(morgan('dev'));
 const allowedOrigins = [
   'http://localhost:3000',
   'https://social-frontend-five.vercel.app',
-  'https://vizzf.vercel.app/'
+  'https://vizzf.vercel.app'
 ];
 
 app.use(cors({
